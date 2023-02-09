694607bfd51ebec9cc85bd1d775d10d00983039b
Apex-Hybrid <101863656+Apex-Hybrid@users.noreply.github.com>
2023-02-08 16:46:56 -0800

    Final commit

--- Js/site.js
+++ Js/site.js
@@ -1,4 +1,37 @@
-//Call Hello World
-function HelloWorld() {
-    alert("Hello World");
+//Get the string from the page
+//contoller function
+function getValues() {
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+//Reverse the string
+//logic function
+function reverseString(userString) {
+
+    let revString = [];
+
+    //reverse a string using a for loop
+    for (let i = userString.length - 1; i >= 0; i--) {
+        revString += userString[i];
+    }
+    return revString;
+}
+
+
+//Display the reversed string
+//View function
+function displayString(revString) {
+
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
